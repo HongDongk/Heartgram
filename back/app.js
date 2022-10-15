4cf47acefc333ffbf60cc4d4f0adc839089a4d50
Dongkeun <hongdongk@naver.com>
2022-10-15 15:09:21 +0900

    Create Database
    npm i nodemon
    create database in mysql from sequlize

--- back/app.js
+++ back/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const postRouter = require('./routes/post');
+const db = require('./models');
+
 
 const app = express();
+db.sequelize.sync()
+  .then(() => {
+    console.log('db 연결 성공');
+  })
+  .catch(console.error);
 
 app.get('/', (req, res) => {
-    res.send('Hello express');
-})
-
-app.use('/post', postRouter);
+  res.send('hello express');
+});
 
 app.listen(3065, () => {
-    console.log('서버 실행 중!');
+  console.log('서버 실행 중!');
 });
--- back/routes/post.js
+++ back/routes/post.js
@@ -1,14 +1,5 @@
-const express = require=('express');
+const express = require('express');
 
 const router = express.Router();
-
-router.post('/', (req, res) => {  // POST / post
-    res.json({id: 1, content: 'hello'});
-});
-
-router.delete('/', (req, res) => {  // DELETE / post
-    res.json({ id: 1 });
-});
-
 
 module.exports = router;
