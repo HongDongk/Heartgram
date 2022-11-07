1911c2e72e1f5f74b99c0fb55ddfd811568e1b7c
Dongkeun <hongdongk@naver.com>
2022-11-07 21:45:17 +0900

    Update config.js

--- back/config/config.js
+++ back/config/config.js
@@ -5,21 +5,21 @@ dotenv.config();
 module.exports = {
   development: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: 'Winner1201!',
     database: 'HeartGram',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   test: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: 'Winner1201!',
     database: 'HeartGram',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: 'Winner1201!',
     database: 'HeartGram',
     host: '127.0.0.1',
     dialect: 'mysql',
