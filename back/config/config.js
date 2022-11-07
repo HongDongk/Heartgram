0deefdbfd6cf379cb5d800fa88361313f894902e
Dongkeun <hongdongk@naver.com>
2022-11-07 21:44:05 +0900

    Update config.js

--- back/config/config.js
+++ back/config/config.js
@@ -3,26 +3,25 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": "HeartGram",
-    "host": "127.0.0.1",
-    "port": "3306",
-    "dialect": "mysql"
+  development: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'HeartGram',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "test": {
-    "username": "root",
-    "password": "1234",
-    "database": process.env.DB_PASSWORD,
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'HeartGram',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "production": {
-    "username": "root",
-    "password": "1234",
-    "database": process.env.DB_PASSWORD,
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'HeartGram',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
 };
