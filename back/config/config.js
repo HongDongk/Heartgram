b8ed9d0c2dd9e059cbf42c829e6acecd3704d2ac
Dongkeun <hongdongk@naver.com>
2022-11-07 21:40:47 +0900

    Update config.js

--- back/config/config.js
+++ back/config/config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": "1234",
+    "password": "Winner1201!",
     "database": process.env.DB_PASSWORD,
     "host": "127.0.0.1",
     "dialect": "mysql"
